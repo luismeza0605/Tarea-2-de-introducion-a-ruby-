12d3b8c46baf63e63842481c21bce1ce8f7bf4f6
*Luis <luismeza1358879@gmail.com>
2024-08-14 17:18:30 +0200

    Ejercicio para el buen uso del hash

---
+++ Ejercicio5_Hash.rb
@@ -0,0 +1,16 @@
+colorHash ={}
+
+colorHash['rojo'] = '#FF0000'
+colorHash['verde']='#00800'
+colorHash['azul']= '#0000FF'
+
+colorHash.each do |tipoCodigo,color|
+  puts tipoCodigo + ':' + color
+end
+
+#ejemplo 2
+
+user = {}
+user = {:name=> "Luis Miguel ", :email=> "luismeza1358879@gmail.com"}
+puts "\n Nombre de Usuario:#{user[:name]}"
+puts "\ncorreo:#{user[:email]}"
