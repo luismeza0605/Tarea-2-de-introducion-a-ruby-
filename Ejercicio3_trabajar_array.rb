861eea2ffcf17e0021e0f54ff4d979700dc02138
*Luis <luismeza1358879@gmail.com>
2024-08-14 16:51:09 +0200

    Ejercicio de los tipos de metodos para mostrar el arry

--- Ejercicio3_trabajar_array.rb
+++ Ejercicio3_trabajar_array.rb
@@ -6,6 +6,9 @@ puts semana
 puts "\nMetodo to_s"
 puts semana.to_s
 
+puts "\nMetodo pop"
+puts semana.pop
+
 puts "\nMetodo join"
 puts semana.join(",")
 
@@ -14,6 +17,9 @@ puts semana.first
 
 puts "\nMetodo last"
 puts semana.last
+
+puts "\nMetodo push"
+puts semana.push "Final"
 
 puts "\nMetodo length"
 puts semana.length
