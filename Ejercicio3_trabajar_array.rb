7a3b25fb7f89bc6e478d529c49d4da3f89d5e6a3
*Luis <luismeza1358879@gmail.com>
2024-08-14 16:49:04 +0200

    Ejercicio de los tipos de metodos para mostrar el arry

---
+++ Ejercicio3_trabajar_array.rb
@@ -0,0 +1,19 @@
+semana=["Lunes","martes","miercoles","jueves","viernes","sabado","domingo"]
+
+puts "Array en ruby"
+puts semana
+
+puts "\nMetodo to_s"
+puts semana.to_s
+
+puts "\nMetodo join"
+puts semana.join(",")
+
+puts "\nMetodo first"
+puts semana.first
+
+puts "\nMetodo last"
+puts semana.last
+
+puts "\nMetodo length"
+puts semana.length
